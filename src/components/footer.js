f14581b56df681346d49b2a135876caf3f7283ad
Markus Schanta <markus@schanta.at>
2021-01-23 22:46:10 +0100

    Fix typo

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ const Footer = ({ content }) => {
   return (
   <>
     <div style={{'font-size': 'smaller', 'text-align': 'center'}}>
-      <p>The sourcode for this page can be found on <a href="https://observablehq.com/@markusschanta/sailing-link">Observable</a> and <a href="https://github.com/markusschanta/sailing">Github</a>.</p>
+      <p>The source code for this page can be found on <a href="https://observablehq.com/@markusschanta/sailing-link">Observable</a> and <a href="https://github.com/markusschanta/sailing">Github</a>.</p>
     </div>
   </>
   )
